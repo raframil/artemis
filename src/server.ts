2a0808d73d2ecbc5836edd10bf15b701c95e3450
Rafael Framil <raframil@gmail.com>
2020-08-03 22:12:28 -0300

    chore: add ts-node-dev

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.get('/test', (request, response) => {
   response.json({ msg: 'OK' });
 });
 
-app.listen(5050);
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
